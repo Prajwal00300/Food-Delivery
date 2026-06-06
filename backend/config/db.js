3b1ed531d27b623d28589a92f276ec7ecc4864e9
Prajwal Kamboj <prajwalkamboj3@gmail.com>
2026-06-06 16:57:04 +0530

    done the changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async ()=> {
-          await mongoose.connect('mongodb+srv://prajwalkamboj3:8307622@cluster0.kk0g5dz.mongodb.net/food-del').then(()=>console.log("DB connected"));
+          await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB connected"));
 } 
