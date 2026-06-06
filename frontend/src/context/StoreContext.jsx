6cd2bcc97f8230658ac748d91aad6ce2285312e9
Prajwal Kamboj <prajwalkamboj3@gmail.com>
2026-06-06 17:23:07 +0530

    added the deployed backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 export const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
-  const url = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"; 
+  const url = import.meta.env.VITE_BACKEND_URL || "https://food-delivery-3qxhk3bzt-prajwals-projects-5bd86beb.vercel.app"; 
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
