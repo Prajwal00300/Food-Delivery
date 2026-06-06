6b53bc0f33568e9558faf7839936e7e154971248
Prajwal Kamboj <prajwalkamboj3@gmail.com>
2026-06-06 16:26:10 +0530

    changes done

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 export const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-delivery-backend-m9u3.onrender.com";
+  const url = "http://localhost:4000"; 
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
